3ce7e8edbd624e0c0c2355482269a1bc013d951c
Ismael Nicolas <ismael.cedillo@outlook.es>
2022-02-23 02:33:39 -0500

    create method

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,44 +1,53 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import Image from "next/image";
 import sun from "src/utils/images/sun.png";
 import moon from "src/utils/images/half-moon.png";
+import Link from "next/link";
+import { setCookies } from 'cookies-next';
+
+const handleSubmint = (e:FormEvent)=>{
+      e.preventDefault();
+      
+}
 
 function Login() {
   return (
-    <div className="LoginS-bodylog">
-    <input type="radio" name="LoginS-dark-ligth" id="LoginS-d-mode" defaultChecked />
-    <label htmlFor="LoginS-d-mode">
-      <Image src={sun} id="LoginS-img" alt=""/>
-    </label>
-    <input type="radio" name="LoginS-dark-ligth" id="LoginS-l-mode" />
-    <label htmlFor="LoginS-l-mode">
-      <Image src={moon} id="LoginS-img" alt=""/>
-    </label>
-    <div className="LoginS-container">
-      <div className="LoginS-boxLog">
-        <h1>Inicia Sesion</h1>
-        <form id="LoginS-form" />
-        <label className="LoginS-labelUser">Usuario</label>
-        <p>
-          <input className="LoginS-userBox" maxLength={100} name="usuario" type="text" />
-        </p>
-        <label className="LoginS-labelPassword">Contraseña</label>
-        <p>
-          <input className="LoginS-userBox" minLength={50} name="password" type="password" />
-        </p>
-        <p>
-          ¿No tiene cuenta? <a href="register">Regístrate</a>
-        </p>
+    <form onSubmit={handleSubmint}>
+        <div className="LoginS-bodylog">
+        <input type="radio" name="LoginS-dark-ligth" id="LoginS-d-mode" defaultChecked />
+        <label htmlFor="LoginS-d-mode">
+          <Image src={sun} id="LoginS-img" alt=""/>
+        </label>
+        <input type="radio" name="LoginS-dark-ligth" id="LoginS-l-mode" />
+        <label htmlFor="LoginS-l-mode">
+          <Image src={moon} id="LoginS-img" alt=""/>
+        </label>
+        <div className="LoginS-container">
+          <div className="LoginS-boxLog">
+            <h1>Inicia Sesion</h1>
+            <form id="LoginS-form" />
+            <label className="LoginS-labelUser">Usuario</label>
+            <p>
+              <input className="LoginS-userBox" maxLength={100} name="usuario" type="text" />
+            </p>
+            <label className="LoginS-labelPassword">Contraseña</label>
+            <p>
+              <input className="LoginS-userBox" minLength={50} name="password" type="password" />
+            </p>
+            <p>
+              ¿No tiene cuenta? <Link href="/register">Regístrate</Link>
+            </p>
+          </div>
+          <div className="LoginS-botones">
+            <input className="LoginS-btn" type="submit" defaultValue="Iniciar Sesion" />
+            <button className="LoginS-btn-google" id="LoginS-btn_google">
+              Inicia con Google
+            </button>
+          </div>
+          <div className="LoginS-logo" />
+        </div>
       </div>
-      <div className="LoginS-botones">
-        <input className="LoginS-btn" type="submit" defaultValue="Iniciar Sesion" />
-        <button className="LoginS-btn-google" id="LoginS-btn_google">
-          Inicia con Google
-        </button>
-      </div>
-      <div className="LoginS-logo" />
-    </div>
-  </div>
+  </form>
   );
 }
 
