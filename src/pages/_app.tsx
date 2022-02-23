fb561a3a8d390d941ccb6a9803d40c93e8a82e5e
kevin chuquimarca <santi.kevin0936@gmail.com>
2022-02-23 10:47:52 -0500

    diseño de la seccion comunity

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import 'src/styles/register.css'
 import 'src/styles/login-style.css'
-import 'semantic-ui-css/semantic.min.css'
+// import 'semantic-ui-css/semantic.min.css'
 
 import type { AppProps } from 'next/app'
 
